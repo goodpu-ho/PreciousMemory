fdf4ea68e06f006a23d0199eeb3e02d591ba7d3a
ho <whdxo10@naver.com>
2020-05-10 15:37:02 +0900

    04/29 Graphql API(4) SendMail Function with Nodemailer.
    	- 이전 commit에서는 임의의 비밀번호를 생성하여 보았다. 이제 생성된 비밀번호를 email로 보내보자.
    
    	1. Nodemailer install : yarn add nodemailer
    
    	2. Utils.js에 sendMail 함수만들기. (파라미터의 email은 email주소만 말하는게 아니라 메일보내는데 필요한 모든 정보를 의미.
    
    	3. Sendgrid 회원가입 -> sendgrid 통해 메일 발신가능
    
    	4. Sendgrid transport 사용위해 yarn add nodemailer-sendgrid-transport 모듈 설치
    
    	5. Fuck sendgrid 안됨!!
    
    	6. Mailgrun으로 전환!  (API, DOMAIN !)

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,44 @@
-import { adjectives, nouns } from "./Word"
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path:path.resolve(__dirname, ".env")});
+
+import { adjectives, nouns } from "./Word";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import mg from 'nodemailer-mailgun-transport';
 
 export const generateSecret = () =>{
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+}
+
+console.log(process.env.MAILGUN_API, process.env.MAILGUN_DOMAIN);
+
+const sendMail = (email) => {
+    const options = {
+        auth: {
+            api_key: process.env.MAILGUN_API,
+			domain: process.env.MAILGUN_DOMAIN
+        }
+    };
+
+    const nodemailerMailgun = nodemailer.createTransport(mg(options));
+    return nodemailerMailgun.sendMail(email, (err, info) => {
+		if (err) {
+			console.log(`Error: ${err}`);
+		} else {
+			console.log(`Response: ${info}`);
+		}
+	});
+};
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from:"ho@prismagram.com",
+        to:address,
+        subject:"🔒 Login Secret for prismagram 🔒",
+        html:`Hello, your login secret is ${secret}. copy paste on the app to login`
+    };
+
+    return sendMail(email);
 }
